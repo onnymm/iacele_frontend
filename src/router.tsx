3f4f8493540e61e1013f9468855989d67dfbf422
onnymm <phr2807@gmail.com>
2025-03-05 09:13:53 -0700

    Cambiada la vista de comisiones por ventas

--- src/router.tsx
+++ src/router.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter(
                     element: <Home />
                 },
                 {
-                    path: '/commissions',
+                    path: '/sales',
                     element: <RouteCommissions />
                 },
                 {
