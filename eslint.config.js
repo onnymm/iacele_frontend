45689379fdb974e8162a5984e8288015dc6272d6
onnymm <phr2807@gmail.com>
2026-06-03 14:02:39 -0700

    Habilitada la regla para poder establecer valores de estados dentro de hooks

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default defineConfig([
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
+      'react-hooks/set-state-in-effect': 'off',
     }
   },
 ])
