ff9bb5f6ab82e8b20dd366bd38035cd5a6501641
onnymm <phr2807@gmail.com>
2026-06-24 13:19:48 -0700

    Envuelto ruteador en componente de fondo negro

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,7 +4,7 @@ import Router from "./router";
 const Root: React.FC = () => {
 
     return (
-        <div className='bg-slate-100 dark:bg-[#101b26] h-full'>
+        <div className='bg-background w-full h-full'>
             <BrowserRouter>
                 <Router />
             </BrowserRouter>
