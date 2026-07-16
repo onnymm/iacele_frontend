8c9a7c0d2f62aeba8a69e70b52a1eb4d550286bc
onnymm <phr2807@gmail.com>
2026-07-16 16:03:25 -0700

    Añadida foto de perfil al componente de perfil de usuario en navbar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -20,14 +20,12 @@ const Navbar = () => {
 
     return (
         <nav id="navbar" className="z-20 sticky flex flex-col gap-2 bg-white dark:bg-[#1f2f3f] shadow p-2 w-full transition select-none">
-            <div id="navbar-header" className="flex flex-row justify-between items-start border h-min min-h-12">
+            <div id="navbar-header" className="flex flex-row justify-between items-start h-min min-h-12">
                 <div/>
                 <div/>
-                <div id="navbar-profile" className="flex flex-row justify-between gap-4 pr-4 border sm:w-72 h-12">
-                    <NavbarProfile />
-                </div>
+                <Settings />
             </div>
-            <div className="flex flex-row justify-between items-center border border-green-500 h-min min-h-12">
+            <div className="flex flex-row justify-between items-center h-min min-h-12">
                 <div id="navbar-main-controls" ref={mainControlsRef}/>
             </div>
         </nav>
@@ -36,21 +34,49 @@ const Navbar = () => {
 
 export default Navbar;
 
+const Separator = () => {
+
+    return (
+        <div
+            className="m-2 border w-[calc(100%) - 1rem]"
+        />
+    );
+};
+
 const NavbarProfile = () => {
 
     // Obtención de los datos del usuario de la sesión
     const { userData } = useUserData();
 
     return (
+        <div id="navbar-profile" className="flex flex-row justify-end items-center gap-2 w-[50%] sm:w-72 h-12">
+            <div className="flex flex-col items-end">
+                <p>{userData.name}</p>
+                {userData.role_ids.length && 
+                    <p className="text-gray-300 text-xs">@{userData.login}</p>
+                }
+            </div>
+            <img src={`data:image/jpeg;base64,${userData.profile_picture}` as string} alt="" className="rounded-full size-10" />
+        </div>
+    );
+};
+
+const Settings = () => {
+
+    return (
         <Popover>
-            <PopoverTrigger asChild>
-                <div>{userData.name}</div>
+            <PopoverTrigger className="cursor-pointer">
+                <NavbarProfile />
             </PopoverTrigger>
-            <PopoverContent>
-                <div className="flex flex-row justify-between items-center">
+            <PopoverContent className="gap-1 p-2 py-4 border">
+                <div className="flex flex-row justify-between items-center px-4">
                     Modo oscuro
                     <DarkModeSwitch />
                 </div>
+                <Separator />
+                <Button>
+                    Mi perfil
+                </Button>
                 <LogoutButton />
             </PopoverContent>
         </Popover>
