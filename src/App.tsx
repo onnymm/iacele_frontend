d3d27802a1294fb6d00e511505f431bdbef50b4f
onnymm <phr2807@gmail.com>
2026-07-23 16:45:44 -0700

    Integradas funciones predeterminadas para notificaciones de toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { Outlet } from "react-router";
 import Navbar from "./components/common/navbar/Navbar";
 import useRedirectToLogin from "./hooks/app/useRedirectToLogin";
 import HeaderControlsBearer from "./components/common/main/AppHeader";
+import useWebsocketNotification from "./hooks/app/useWebsocketNotification";
+import showToast from "./components/ui/toast/toast";
+import { KeyRound } from "lucide-react";
 
 const App = () => {
 
     useRedirectToLogin();
+
+    useDefaultToastNotifications();
 
     return (
         <>
@@ -17,3 +22,18 @@ const App = () => {
 };
 
 export default App;
+
+const useDefaultToastNotifications = () => {
+
+    useWebsocketNotification(
+        'password.changed',
+        () => {
+            showToast({
+                title: 'Cambio de contraseña',
+                content: 'La contraseña ha sido cambiada.',
+                icon: KeyRound,
+                type: 'warning',
+            })
+        },
+    );
+};
