9506e51e79371401d508390d1e8cde220f419860
onnymm <phr2807@gmail.com>
2026-07-29 16:12:06 -0700

    Creado nuevo tipado para segunda versión de formulario

---
+++ src/types/iacelev2.d.ts
@@ -0,0 +1,440 @@
+declare namespace IACeleV2 {
+
+    declare namespace API {
+
+        declare namespace Request {
+
+            declare namespace _Definition {
+
+                interface _RequiresModelName <M extends Data.ModelName>{
+                    'model_name': M;
+                };
+
+                interface _RequiresRecordIDs {
+                    'record_ids': Typing.ScalarOrArray<number>;
+                };
+                
+                interface _RequiresRecordID {
+                    'record_id': number;
+                };
+
+                interface _SupportsFieldsRead<M extends Data.ModelName> {
+                    'fields': Data.ReadField<M>[];
+                };
+
+                interface _RequiresRecordData <M extends Data.ModelName>{
+                    'data': Data.EditableRecord<M>;
+                };
+
+                declare namespace Base {
+
+                    type Update<M extends Data.ModelName> = (
+                        & _RequiresModelName<M>
+                        & _RequiresRecordIDs
+                        & _RequiresRecordData<M>
+                    );
+
+                    type Delete<M extends Data.ModelName> = (
+                        & _RequiresModelName<M>
+                        & _RequiresRecordIDs
+                    );
+
+                    type Form<M extends Data.ModelName> = (
+                        & _RequiresModelName<M>
+                        & _SupportsFieldsRead<M>
+                        & _RequiresRecordID
+                    );
+
+                };
+
+            };
+
+            type Update<M extends Data.ModelName> = _Definition.Base.Update<M>;
+
+            type Delete<M extends Data.ModelName> = _Definition.Base.Delete<M>;
+
+            type Form<M extends Data.ModelName> = _Definition.Base.Form<M>;
+
+            type FieldsMetadata<M extends Data.ModelName> = _Definition._RequiresModelName<M>;
+
+        };
+
+        declare namespace Response {
+
+            type Update = true;
+
+            type Delete = true;
+
+            type FieldsMetadata<M extends Data.ModelName> = IACeleV2.Data.FieldMetadata<M>[];
+
+            interface Form<M extends Data.ModelName> {
+                'name': string;
+                'record': Data.RecordFromDatabase<M>;
+            };
+
+        };
+
+    };
+
+    declare namespace Data {
+
+        declare namespace _Definition {
+
+            declare namespace _RelationCommand {
+
+                type Create<M extends ModelName> = EditableRecord<M>[];
+                type Update<M extends ModelName> = [number[], EditableRecord<M>];
+                type Add = [number[]];
+                type Unlink = [number[]];
+                type Delete = [number[]];
+                type Replace = [number[]];
+                type Clear = [];
+
+            };
+
+            interface _CommonFieldsProperties {
+                id: TType.Integer<'not_null'>;
+                name: TType.Char;
+                create_date: TType.Datetime<'not_null'>;
+                update_date: TType.Datetime<'not_null'>;
+                create_uid: TType.Many2One<'not_null'>;
+                update_uid: TType.Many2One<'not_null'>;
+                display_name: TType.Char<'not_null'>;
+            };
+
+            type ArrayTTypeName = 'one2many' | 'many2many';
+
+            type RelationCommand<M extends ModelName> = {
+                'create'?: _RelationCommand.Create<M>;
+                'update'?: _RelationCommand.Update<M>;
+                'add'?: _RelationCommand.Add;
+                'unlink'?: _RelationCommand.Unlink;
+                'delete'?: _RelationCommand.Delete;
+                'replace'?: _RelationCommand.Replace;
+                'clear'?: _RelationCommand.Clear;
+            };
+
+        };
+
+        declare namespace TType {
+
+            declare namespace _Definition {
+
+                type NullityKey = "not_null" | "null_";
+
+                interface _WithNullOption<T>{
+                    'null_': T | null;
+                    'not_null': T;
+                };
+
+                type TTypeName = (
+                    | 'integer'
+                    | 'char'
+                    | 'boolean'
+                    | 'float'
+                    | 'date'
+                    | 'datetime'
+                    | 'time'
+                    | 'duration'
+                    | 'selection'
+                    | 'text'
+                    | 'file'
+                    | 'many2one'
+                    | 'one2many'
+                    | 'many2many'
+                    | 'json'
+                );
+
+                interface ShapeVariant<N extends _Definition.NullityKey, D, V, E, T extends TTypeName, M extends ModelName = null> {
+                    'ttype': T,
+                    'database': _WithNullOption<D>[N];
+                    'view': _WithNullOption<V>[N];
+                    'edition': _WithNullOption<E>[N];
+                    'modelName': M;
+                };
+
+                declare namespace _JSON {
+
+                    type _Serializable = number | string | boolean | null;
+                    type _JSONObject = Record<string, _Serializable>;
+                    type _ScalarOrArray<T> = T | T[];
+                };
+
+                type JSON = _JSON._ScalarOrArray<_JSON._Serializable | _JSON._JSONObject>;
+
+            };
+
+            type Integer<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, number, number, number, 'integer'>;
+            type Char<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'char'>;
+            type Boolean<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, boolean, boolean, boolean, 'boolean'>;
+            type Float<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, number, number, number, 'float'>;
+            type Date<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'date'>;
+            type Datetime<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'datetime'>;
+            type Time<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'time'>;
+            type Duration<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'duration'>;
+            type Selection<O extends string, N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, O, O, O, 'selection'>;
+            type Text<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'text'>;
+            type File<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, string, string, string, 'file'>;
+            type Many2One<N extends _Definition.NullityKey = 'null_'> = _Definition.ShapeVariant<N, [number, string], [number, string], number, 'many2one'>;
+            type One2Many<M extends ModelName> = _Definition.ShapeVariant<'not_null', ModelDefinition<M>[], ModelDefinition<M>[], number, 'one2many', M>
+            type Many2Many<M extends ModelName> = _Definition.ShapeVariant<'not_null', ModelDefinition<M>[], ModelDefinition<M>[], number, 'many2many', M>
+            type JSON = _Definition.ShapeVariant<'not_null', _Definition.JSON, _Definition.JSON, _Definition.JSON, 'json'>;
+
+        };
+
+        interface Model {
+
+            'base.model': {
+                state: TType.Selection<'base' | 'generic', 'not_null'>;
+                label: TType.Char;
+                model: TType.Char;
+                has_sequence: TType.Boolean<'not_null'>;
+                is_archivable: TType.Boolean<'not_null'>;
+                has_label: TType.Boolean<'not_null'>;
+                description: TType.Text;
+                transient: TType.Boolean<'not_null'>;
+                field_ids: TType.One2Many<'base.model.field'>;
+                related_field_ids: TType.One2Many<'base.model.field'>;
+            };
+
+            'base.model.field': {
+                state: TType.Selection<'base' | 'generic', 'not_null'>;
+                label: TType.Char<'not_null'>;
+                model_id: TType.Many2One<'not_null'>;
+                ttype: TType.Selection<TTypeName, 'not_null'>;
+                nullable: TType.Boolean<'not_null'>;
+                on_delete: TType.Selection<'cascade' | 'restrict' | 'set_null'>;
+                is_required: TType.Boolean<'not_null'>;
+                readonly: TType.Boolean<'not_null'>;
+                default_value: TType.JSON;
+                unique: TType.Boolean<'not_null'>;
+                help_info: TType.Text;
+                related_model_id: TType.Many2One;
+                related_field: TType.Char;
+                is_computed: TType.Boolean<'not_null'>;
+                selection_ids: TType.One2Many<'base.model.field.selection'>;
+            };
+
+            'base.model.field.selection': {
+                label: TType.Char<'not_null'>;
+                field_id: TType.Many2One<'not_null'>;
+            };
+
+            'base.users': {
+                active: TType.Boolean<'not_null'>;
+                login: TType.Char<'not_null'>;
+                password: TType.Char<'not_null'>;
+                profile_picture: TType.File;
+                role_ids: TType.Many2Many<'base.users.role'>;
+            };
+
+            'base.users.role': {
+                label: TType.Char<'not_null'>;
+                group_ids: TType.Many2Many<'base.user.groups'>;
+            };
+
+            'base.user.groups': {
+                label: TType.Char<'not_null'>;
+                access_id: TType.One2Many<'base.user.access'>;
+            };
+
+            'base.user.access': {
+                model_id: TType.Many2One<'not_null'>;
+                perm_create: TType.Boolean<'not_null'>;
+                perm_read: TType.Boolean<'not_null'>;
+                perm_update: TType.Boolean<'not_null'>;
+                perm_delete: TType.Boolean<'not_null'>;
+                group_id: TType.Many2One<'not_null'>;
+            };
+
+            'base.users.update.password': {
+                current_password: TType.Char;
+                new_password: TType.Char;
+                confirm_password: TType.Char;
+            };
+
+            'location.warehouse': {
+                short_name: TType.Char<'not_null'>;
+                location_number: TType.Integer<'not_null'>;
+            };
+
+            'resource.device.type': {};
+
+            'schedule.week': {
+                weekday: TType.Selection<'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday', 'not_null'>;
+                start_time: TType.Time<'not_null'>;
+                end_time: TType.Time<'not_null'>;
+            };
+
+            'model.sync': {
+                last_sync: TType.Datetime<'not_null'>;
+                model_id: TType.Many2One<'not_null'>;
+            };
+
+            'resource.device': {
+                model: TType.Char;
+                brand: TType.Char;
+                serial_number: TType.Char;
+                firmware_version: TType.Char;
+                type_id: TType.Many2One<'not_null'>;
+                location_id: TType.Many2One;
+            };
+
+            'hr.employee': {
+                active: TType.Boolean<'not_null'>;
+                odoo_id: TType.Integer;
+                hire_date: TType.Date;
+                location_id: TType.Many2One;
+                user_id: TType.Many2One;
+            };
+
+            'schedule.week.offset': {
+                employee_id: TType.Many2One<'not_null'>;
+                start_offset: TType.Duration<'not_null'>;
+                end_offset: TType.Duration<'not_null'>;
+                weekday: TType.Selection<'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday', 'not_null'>;
+            };
+
+            'assistance.registry.day': {
+                date: TType.Date<'not_null'>;
+                employee_id: TType.Many2One<'not_null'>;
+                schedule_id: TType.Many2One;
+                offset_id: TType.Many2One;
+                event_ids: TType.One2Many<'assistance.registry.event'>;
+                start_time: TType.Time;
+                end_time: TType.Time;
+                lunch_time: TType.Duration;
+                weekday: TType.Char;
+                allowed_start: TType.Time;
+                allowed_end: TType.Time;
+                late_start: TType.Duration;
+                early_end: TType.Duration;
+                is_complete: TType.Boolean;
+                has_valid_events: TType.Boolean;
+            };
+
+
+            'assistance.registry.event': {
+                employee_id: TType.Many2One<'not_null'>;
+                original_registry_time: TType.Datetime<'not_null'>;
+                original_status: TType.Selection<'undefined', 'check_in', 'break_out', 'break_in', 'check_out'>;
+                device_id: TType.Many2One;
+                from_api: TType.Boolean<'not_null'>;
+                registry_time_correction: TType.Datetime;
+                status_correction: TType.Selection<'null', 'undefined', 'check_in', 'break_out', 'break_in', 'check_out'>;
+                day_id: TType.Many2One;
+                registry_time: TType.Datetime;
+                status: TType.Selection<'null', 'undefined', 'check_in', 'break_out', 'break_in', 'check_out'>;
+                has_corrections: TType.Boolean<'not_null'>;
+                correction_history_ids: TType.One2Many<'assistance.registry.event.correction.historial'>;
+            };
+
+            'assistance.registry.event.correction.historial': {
+                event_id: TType.Many2One<'not_null'>;
+                move_type: TType.Selection<'correction' | 'undo', 'not_null'>;
+                new_status: TType.Selection<'null', 'undefined', 'check_in', 'break_out', 'break_in', 'check_out'>;
+                new_registry_time: TType.Datetime;
+            };
+
+            'assistance.registry.event.correction': {
+                event_id: TType.Many2One<'not_null'>;
+                status: TType.Selection<'null', 'undefined', 'check_in', 'break_out', 'break_in', 'check_out'>;
+                registry_time: TType.Datetime;
+            };
+
+            'assistance.registry.event.credentials': {
+                token: TType.Char<'not_null'>;
+                cookie_uuid: TType.Char<'not_null'>;
+                site_id: TType.Char<'not_null'>;
+            };
+
+        };
+
+        type ModelName = keyof Model;
+
+        type TTypeName = TType._Definition.TTypeName;
+
+        interface FieldMetadata <M extends ModelName>{
+            id: number;
+            name: FieldName<M>;
+            label: string;
+            ttype: TType._Definition.TTypeName;
+            help_info: string;
+            related_model: ModelName;
+            selection_ids: {
+                id: number;
+                name: string;
+                label: string;
+            }[];
+            readonly: boolean;
+            is_computed: boolean;
+        };
+
+        type FieldsMetadata<M extends ModelName> = Record<FieldName<M>, FieldMetadata<M>>
+
+        type ModelsMetadata<M extends ModelName> = Partial<Record<ModelName, FieldsMetadata<M>>>
+
+        type ModelDefinition<M extends ModelName> = _Definition._CommonFieldsProperties & Model[M];
+
+        type RecordFromDatabase<M extends ModelName> = {
+            [K in keyof ModelDefinition<M>]: (
+                ModelDefinition<M>[K]['ttype'] extends _Definition.ArrayTTypeName
+                    ? RecordFromDatabase<ModelDefinition<M>[K]['modelName']>[]
+                    : ModelDefinition<M>[K]['database']
+            );
+        };
+
+        type EditableRecord<M extends ModelName> = {
+            [K in keyof ModelDefinition<M>]?: (
+                ModelDefinition<M>[K]['ttype'] extends _Definition.ArrayTTypeName
+                    ? RelationCommand<ModelDefinition<M>[K]['modelName']>
+                    : ModelDefinition<M>[K]['edition']
+            );
+        };
+
+        type RecordForView<M extends ModelName> = {
+            [K in keyof ModelDefinition<M>]: (
+                ModelDefinition<M>[K]['ttype'] extends _Definition.ArrayTTypeName
+                    ? RelationCommand<ModelDefinition<M>[K]['modelName']>
+                    : ModelDefinition<M>[K]['view']
+            );
+        };
+
+        type RelationCommand<M extends ModelName> = _Definition.RelationCommand<M>
+
+        type ScalarFieldName<M extends ModelName> = {
+            [K in keyof ModelDefinition<M>]: (
+                ModelDefinition<M>[K]['ttype'] extends _Definition.ArrayTTypeName
+                    ? never
+                    : K
+            );
+        }[keyof ModelDefinition<M>];
+
+        type ArrayyFieldName<M extends ModelName> = {
+            [K in keyof ModelDefinition<M>]: (
+                ModelDefinition<M>[K]['ttype'] extends _Definition.ArrayTTypeName
+                    ? K
+                    : never
+            );
+        }[keyof ModelDefinition<M>];
+
+        type FieldName<M extends ModelName> = keyof ModelDefinition<M>;
+
+        type TTypeField<M extends ModelName, L extends TType._Definition.TTypeName> = {
+            [K in keyof ModelDefinition<M>]: (
+                ModelDefinition<M>[K]['ttype'] extends L
+                    ? K
+                    : never
+            )
+        }[keyof ModelDefinition<M>];
+
+        type ExpandedRelation<M extends Data.ModelName, F extends Data.ArrayyFieldName<M>> = [
+            F,
+            Data.FieldName<Data.ModelDefinition<M>[F]['modelName']>[],
+        ];
+
+        type ReadField<M extends Data.ModelName> = Data.FieldName<M> | ExpandedRelation<M, Data.ArrayyFieldName<M>>;
+
+    };
+
+};
