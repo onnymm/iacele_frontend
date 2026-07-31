159319d8eda6840b9ba66b64e1909b1d8a6e059b
onnymm <phr2807@gmail.com>
2026-07-31 10:43:37 -0700

    Implementado hook para edición y visualización de registro para vistas

---
+++ src/hooks/views/useRecordEdition.tsx
@@ -0,0 +1,48 @@
+import { useCallback } from "react";
+import useEditableRecord from "./useEditableRecord";
+import useOriginalRecord from "./useOriginalRecord"
+import useRecordInView from "./useRecordInView";
+
+const useRecordEdition = <M extends IACeleV2.Data.ModelName>() => {
+
+    // Obtención de valores desde los hooks
+    const { deleteOriginalRecord } = useOriginalRecord<M>();
+    const { recordInView, updateRecordInViewField, undoChangesInRecordInView } = useRecordInView<M>();
+    const { updateEditableRecordField, undoChangesInEditableRecord, existingChanges, updateRecord } = useEditableRecord<M>();
+
+    // Función para modificación de valor de campo en registros de edición y vista
+    const updateRecordField = useCallback(
+        <F extends IACeleV2.Data.FieldName<M>>(
+            fieldName: F,
+            value: IACeleV2.Data.RecordForView<M>[F],
+        ) => {
+
+            // Actualización de valor en registro de vista
+            updateRecordInViewField<F>(fieldName, value);
+            // Actualización de valor en registro de edición
+            updateEditableRecordField<F>(fieldName, value);
+        }, [updateEditableRecordField, updateRecordInViewField]
+    );
+
+    // Función para deshacer cambios
+    const undoChanges = useCallback(
+        () => {
+            
+            // Se deshacen cambios en el registro de vista
+            undoChangesInRecordInView();
+            // Se deshacen cambios en el registro de edición
+            undoChangesInEditableRecord();
+        }, [undoChangesInEditableRecord, undoChangesInRecordInView]
+    );
+
+    return {
+        recordInView,
+        updateRecordField,
+        existingChanges,
+        undoChanges,
+        updateRecord,
+        deleteRecord: deleteOriginalRecord,
+    };
+};
+
+export default useRecordEdition;
