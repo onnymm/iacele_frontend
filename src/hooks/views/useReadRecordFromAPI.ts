8f9ce290e85307cb099e22a0d5adcf9fe46d500f
onnymm <phr2807@gmail.com>
2026-08-05 12:32:34 -0700

    Desacoplada funcionalidad de suscripción de campos a leer a hook propio

--- src/hooks/views/useReadRecordFromAPI.ts
+++ src/hooks/views/useReadRecordFromAPI.ts
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useAPI from "../app/useAPI";
 import useReload from "../app/useReload";
 import useDataView from "../routes/useDataView";
 import useGetModelNameFromView from "./useGetModelNameFromView";
+import useSuscribeFieldsToRead from "./useSuscribeFieldsToRead";
 
 const useReadRecordFromAPI = <M extends IACeleV2.Data.ModelName>() => {
 
@@ -17,43 +18,11 @@ const useReadRecordFromAPI = <M extends IACeleV2.Data.ModelName>() => {
     // Inicialización de estado de datos del registro de la base de datos
     const [ dataFromAPI, setDataFromAPI ] = useState<IACeleV2.Data.RecordFromDatabase<M> | null>(null);
     // Inicialización de lista de campos a leer
-    const fieldsToRead = useRef<IACeleV2.Data.ReadField<M>[]>([]);
-
-    // Función para suscribir campo para lectura
-    const suscribeFieldToRead = useCallback(
-        (fieldName: IACeleV2.Data.ReadField<M>) => {
-            // Se busca el valor del campo en el array
-            const foundValue = fieldsToRead.current.find(
-                ( suscribedFieldName ) => {
-                    // Si el tipo de dato de los campos a comparar es string...
-                    if ( typeof fieldName === 'string' && typeof suscribedFieldName === 'string' ) {
-                        // Se busca igualdad con los campos
-                        return ( suscribedFieldName === fieldName );
-
-                    // Si el tipo de dato de los campos a comparar es array...
-                    } else if ( typeof fieldName === 'object' && typeof suscribedFieldName === 'object' ) {
-                        // Extracción del campo expandido suscrito
-                        const expandedSuscribedField = suscribedFieldName[0];
-                        // Extracción del campo expandido a suscribir
-                        const expandedField = fieldName[0];
-                        // Se busca igual con los campos
-                        return ( expandedSuscribedField === expandedField );
-                    };
-
-                    return false;
-                },
-            );
-            // Si el nombre del campo no existe en el array...
-            if ( !foundValue ) {
-                // Se añade éste
-                fieldsToRead.current.push(fieldName);
-            };
-        }, []
-    );
+    const { fieldsToRead, suscribeFieldToRead } = useSuscribeFieldsToRead<M>();
 
     // Función para leer el registro
     const read = useCallback(
-        async () => {
+        async (fieldsToRead: React.RefObject<IACeleV2.Data.ReadField<M>[]>) => {
             // Obtención de los datos desde la API
             const data = await api.formv2({
                 'model_name': modelName,
@@ -93,8 +62,8 @@ const useReadRecordFromAPI = <M extends IACeleV2.Data.ModelName>() => {
     // Efecto para ejecutar la función de lectura
     useEffect(
         () => {
-            read();
-        }, [read, reloadSignal]
+            read(fieldsToRead);
+        }, [read, reloadSignal, fieldsToRead]
     );
 
     return {
---
+++ src/hooks/views/useSuscribeFieldsToRead.ts
@@ -0,0 +1,43 @@
+import { useCallback, useRef } from "react";
+
+const useSuscribeFieldsToRead = <M extends IACeleV2.Data.ModelName>() => {
+
+    // Inicialización de lista de campos a leer
+    const fieldsToRead = useRef<IACeleV2.Data.ReadField<M>[]>([]);
+
+    // Función para suscribir campo para lectura
+    const suscribeFieldToRead = useCallback(
+        (fieldName: IACeleV2.Data.ReadField<M>) => {
+            // Se busca el valor del campo en el array
+            const foundValue = fieldsToRead.current.find(
+                ( suscribedFieldName ) => {
+                    // Si el tipo de dato de los campos a comparar es string...
+                    if ( typeof fieldName === 'string' && typeof suscribedFieldName === 'string' ) {
+                        // Se busca igualdad con los campos
+                        return ( suscribedFieldName === fieldName );
+
+                    // Si el tipo de dato de los campos a comparar es array...
+                    } else if ( typeof fieldName === 'object' && typeof suscribedFieldName === 'object' ) {
+                        // Extracción del campo expandido suscrito
+                        const expandedSuscribedField = suscribedFieldName[0];
+                        // Extracción del campo expandido a suscribir
+                        const expandedField = fieldName[0];
+                        // Se busca igual con los campos
+                        return ( expandedSuscribedField === expandedField );
+                    };
+
+                    return false;
+                },
+            );
+            // Si el nombre del campo no existe en el array...
+            if ( !foundValue ) {
+                // Se añade éste
+                fieldsToRead.current.push(fieldName);
+            };
+        }, []
+    );
+
+    return { fieldsToRead, suscribeFieldToRead };
+};
+
+export default useSuscribeFieldsToRead;
