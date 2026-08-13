cf38f84a77bd64ae939814072a7f61a9e7b14d6c
onnymm <phr2807@gmail.com>
2026-08-13 11:48:13 -0700

    Correcciones en componentes booleanos Checkbox y Switch

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -12,14 +12,14 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer relative flex size-4 shrink-0 items-center justify-center rounded-[5px] border border-transparent bg-input/90 transition-shadow outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/30 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-primary data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary",
+        "peer after:absolute relative after:-inset-x-3 after:-inset-y-2 flex justify-center items-center bg-input/90 data-checked:bg-primary dark:data-checked:bg-primary disabled:opacity-50 group-has-disabled/field:opacity-50 border border-transparent data-checked:border-primary aria-invalid:aria-checked:border-primary aria-invalid:border-destructive focus-visible:border-ring dark:aria-invalid:border-destructive/50 rounded-[5px] outline-none aria-invalid:ring-3 aria-invalid:ring-destructive/20 focus-visible:ring-3 focus-visible:ring-ring/30 dark:aria-invalid:ring-destructive/40 size-4 data-checked:text-primary-foreground transition-shadow disabled:pointer-events-none shrink-0",
         className
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="grid place-content-center text-current transition-none [&>svg]:size-3.5"
+        className="place-content-center grid [&>svg]:size-3.5 text-current transition-none"
       >
         <CheckIcon
         />
